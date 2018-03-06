2b3fba7322b4a1da72e7d985b3fb9051ff4bbd9e
GabbyMont <gemo1236@gmail.com>
2018-03-05 21:55:47 -0500

    Pairing test

---
+++ pairing_test.rb
@@ -0,0 +1,18 @@
+require "minitest/autorun"
+require_relative "pairing_function.rb"
+
+class Pair_generator < Minitest::Test
+
+	def test_1_equals_1
+		assert_equal(1,1)
+	end
+
+	def test_array
+		print random_array(["Adam", "Tristan", "Fabio", "Hunter"])
+		assert_equal(2, random_array(["Adam", "Tristan", "Fabio", "Hunter"]).count)
+	end
+
+	# def test_1_shuffle
+	# 	assert_equal()
+	# end
+end
