b9c5caa4e6ecd4123b2d66e6414f00bb58ed2225
GabbyMont <gemo1236@gmail.com>
2018-03-05 22:23:43 -0500

    test_2_shuffle: pairing even amount of names

--- pairing_test.rb
+++ pairing_test.rb
@@ -7,13 +7,19 @@ class Pair_generator < Minitest::Test
 		assert_equal(1,1)
 	end
 
-	def test_array
-		print random_array(["Adam", "Tristan", "Fabio", "Hunter"])
-		assert_equal(2, random_array(["Adam", "Tristan", "Fabio", "Hunter"]).count)
+	# def test_array
+	# 	print random_array(["Adam", "Tristan", "Fabio", "Hunter"])
+	# 	assert_equal(2, random_array(["Adam", "Tristan", "Fabio", "Hunter"]).count)
+	# end
+
+	# def test_1_shuffle
+	# 	print random_array(["Autym", "Casey", "Jason", "Evan", "Jack"])
+	# 	assert_equal(2, random_array(["Autym", "Casey", "Jason", "Evan", "Jack"]).count)
+	# end
+
+	def test_2_shuffle
+		print random_array(["Eden", "Persephone", "Celeste", "Leopold", "Amethyst", "Davy", "Cosmo", "Loki"])
+		assert_equal(4,random_array(["Eden", "Persephone", "Celeste", "Leopold", "Amethyst", "Davy", "Cosmo", "Loki"]).count)
 	end
 
-	def test_1_shuffle
-		print random_array(["Autym", "Casey", "Jason", "Evan", "Jack"])
-		assert_equal(2, random_array(["Autym", "Casey", "Jason", "Evan", "Jack"]).count)
-	end
 end
